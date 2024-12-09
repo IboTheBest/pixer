634f9b3830e77cdeb2b4d21eb5e87e1894d8df78
IboTheBest <cybergenyus@gmail.com>
2024-12-09 17:10:00 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,42 @@
 function handleModeBtnClick() {
-    document.body.classList.toggle("dark")
+  const wasAdded = document.body.classList.toggle("dark");
+    if (wasAdded) {
+      setCookie("darkMode", "enabled", 7)
+    } else {
+      setCookie("darkMode", "disabled", 7);
 }
+}
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+  return null;
+}
+
+
+function setCookie(name, value, days) {
+  const date = new Date();
+  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+  const expires = `expires=${date.toUTCString()}`;
+  document.cookie = `${name}=${value}; ${expires}; path=/`;
+}
+if (getCookie('darkMode') === 'enabled') {
+  themeLink.href = 'dark-theme.css';
+}
+
+function applyTheme() {
+  const userTheme = getCookie('darkMode'); 
+  if (userTheme === 'enabled') {
+    handleModeBtnClick()
+  } else {
+    
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      themeLink.href = 'dark-theme.css'; 
+    }
+  }
+}
+
+toggleBtn.addEventListener('click', toggleDarkMode);
+
+applyTheme();
